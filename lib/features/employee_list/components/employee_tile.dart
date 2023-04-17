db91605da6e175ddc8ac4d66450a14d6bbc48027
lakshydeep-14 <lvikramsah@gmail.com>
2023-04-17 12:50:38 +0545

    employee list added

--- lib/features/employee_list/components/employee_tile.dart
+++ lib/features/employee_list/components/employee_tile.dart
@@ -1,45 +1,95 @@
+import 'package:employee/controller/db.dart';
+import 'package:employee/features/add_or_edit_employee/edit_employee.dart';
+import 'package:employee/main.dart';
 import 'package:employee/utils/app_extension.dart';
+import 'package:intl/intl.dart';
 
-class EmployeeTile extends StatelessWidget {
-  final String name, date, role;
-  const EmployeeTile(
-      {super.key, required this.name, required this.date, required this.role});
+class EmployeeTile extends StatefulWidget {
+  final Employee employee;
+  const EmployeeTile({super.key, required this.employee});
 
   @override
+  State<EmployeeTile> createState() => _EmployeeTileState();
+}
+
+class _EmployeeTileState extends State<EmployeeTile> {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: AppColors.white,
-      margin: const EdgeInsets.only(bottom: 1.5),
-      child: ListTile(
-        contentPadding: const EdgeInsets.all(16),
-        title: Text(
-          name,
-          style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
+    return GestureDetector(
+      onTap: () {
+        Get.to(EditEmployeeScreen(employee: widget.employee));
+      },
+      child: Dismissible(
+        background: Container(
+          color: Colors.red,
+          margin: const EdgeInsets.symmetric(horizontal: 15),
+          padding: const EdgeInsets.only(right: 20),
+          alignment: Alignment.centerRight,
+          child: const Icon(
+            Icons.delete,
+            color: Colors.white,
+          ),
         ),
-        subtitle: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(
-              height: 6,
+        key: Key(widget.employee.id.toString() + DateTime.now().toString()),
+        direction: DismissDirection.endToStart,
+        onDismissed: (_) async {
+          await databaseManager.deleteemployee(widget.employee.id!);
+          databaseManager.fetchAllEmployee();
+        },
+        child: Container(
+          color: AppColors.white,
+          margin: const EdgeInsets.only(bottom: 1.5),
+          child: ListTile(
+            contentPadding: const EdgeInsets.all(16),
+            title: Text(
+              widget.employee.name!,
+              style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
             ),
-            Text(
-              role,
-              style: const TextStyle(
-                  color: Color(0xff949C9E),
-                  fontWeight: FontWeight.w400,
-                  fontSize: 14),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 6,
+                ),
+                Text(
+                  widget.employee.role!,
+                  style: const TextStyle(
+                      color: Color(0xff949C9E),
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14),
+                ),
+                const SizedBox(
+                  height: 6,
+                ),
+                Text(
+                  "From " +
+                      dateFormattor(DateTime.parse(widget.employee.fromDate!)),
+                  style: const TextStyle(
+                      color: Color(0xff949C9E),
+                      fontWeight: FontWeight.w400,
+                      fontSize: 12),
+                ),
+                (widget.employee.toDate == "noDate" ||
+                        widget.employee.toDate == '')
+                    ? const Text(
+                        "  ",
+                        style: TextStyle(
+                            color: Color(0xff949C9E),
+                            fontWeight: FontWeight.w400,
+                            fontSize: 12),
+                      )
+                    : Text(
+                        " - " +
+                            dateFormattor(
+                                DateTime.parse(widget.employee.toDate!)),
+                        style: const TextStyle(
+                            color: Color(0xff949C9E),
+                            fontWeight: FontWeight.w400,
+                            fontSize: 12),
+                      ),
+              ],
             ),
-            const SizedBox(
-              height: 6,
-            ),
-            Text(
-              date,
-              style: const TextStyle(
-                  color: Color(0xff949C9E),
-                  fontWeight: FontWeight.w400,
-                  fontSize: 12),
-            ),
-          ],
+          ),
         ),
       ),
     );
--- lib/features/employee_list/employee_list.dart
+++ lib/features/employee_list/employee_list.dart
@@ -1,5 +1,7 @@
 import 'package:employee/features/employee_list/components/employee_tile.dart';
+import 'package:employee/main.dart';
 
+import '../../controller/db.dart';
 import '../add_or_edit_employee/add_employee.dart';
 import 'package:employee/utils/app_extension.dart';
 
@@ -12,39 +14,112 @@ class EmployeeListScreen extends StatefulWidget {
 
 class _EmployeeListScreenState extends State<EmployeeListScreen> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  Future<List<Employee>> fetchEmployeesFromDatabase() async {
+    Future<List<Employee>> employees = databaseManager.fetchAllEmployee();
+    return employees;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: const [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 16),
-            child: Text(
-              "Current employees",
-              style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  color: AppColors.primary,
-                  fontSize: 16),
-            ),
-          ),
-          EmployeeTile(name: "Lakshydeep", role: "Flutter", date: "678-780678"),
-          EmployeeTile(name: "Lakshydeep", role: "Flutter", date: "678-780678"),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 16),
-            child: Text(
-              "Previous employees",
-              style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  color: AppColors.primary,
-                  fontSize: 16),
-            ),
-          ),
-          EmployeeTile(name: "Lakshydeep", role: "Flutter", date: "678-780678"),
-          EmployeeTile(name: "Lakshydeep", role: "Flutter", date: "678-780678"),
-        ],
+      body: FutureBuilder<List<Employee>>(
+        future: databaseManager.fetchAllEmployee(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            if (!snapshot.hasData == null || snapshot.data!.isEmpty) {
+              return Center(
+                child: SvgPicture.asset('assets/no_employee.svg'),
+              );
+            }
+            return ListView(
+              children: [
+                if (snapshot.data!
+                    .where((element) =>
+                        element.toDate != "noDate" && element.toDate != "")
+                    .isNotEmpty)
+                  const Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 16.0, vertical: 16),
+                    child: Text(
+                      "Current employees",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          color: AppColors.primary,
+                          fontSize: 16),
+                    ),
+                  ),
+                if (snapshot.data!
+                    .where((element) =>
+                        element.toDate != "noDate" && element.toDate != "")
+                    .isNotEmpty)
+                  ListView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: snapshot.data!
+                          .where((element) =>
+                              element.toDate != "noDate" &&
+                              element.toDate != "")
+                          .toList()
+                          .length,
+                      itemBuilder: (context, i) {
+                        var data = snapshot.data!
+                            .where((element) =>
+                                element.toDate != "noDate" &&
+                                element.toDate != "")
+                            .toList()[i];
+                        return EmployeeTile(employee: data);
+                      }),
+                if (snapshot.data!
+                    .where((element) =>
+                        element.toDate == "noDate" || element.toDate == "")
+                    .isNotEmpty)
+                  const Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 16.0, vertical: 16),
+                    child: Text(
+                      "Previous employees",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          color: AppColors.primary,
+                          fontSize: 16),
+                    ),
+                  ),
+                if (snapshot.data!
+                    .where((element) =>
+                        element.toDate == "noDate" || element.toDate == "")
+                    .isNotEmpty)
+                  ListView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: snapshot.data!
+                          .where((element) =>
+                              element.toDate == "noDate" ||
+                              element.toDate == "")
+                          .toList()
+                          .length,
+                      itemBuilder: (context, i) {
+                        var data = snapshot.data!
+                            .where((element) =>
+                                element.toDate == "noDate" ||
+                                element.toDate == "")
+                            .toList()[i];
+                        return EmployeeTile(employee: data);
+                      }),
+              ],
+            );
+          } else {
+            return Container(
+              alignment: AlignmentDirectional.center,
+              child: const CircularProgressIndicator(),
+            );
+          }
+        },
       ),
-      // Center(
-      //   child: SvgPicture.asset('assets/no_employee.svg'),
-      // ),
       floatingActionButton: FloatingActionButton(
           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
           child: const Icon(
